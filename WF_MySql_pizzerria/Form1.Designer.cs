122d5bfd8237ee641499e49c1f60d5d034a6d649
Aluno <Aluno@SCS14>
2022-09-15 20:15:47 -0300

    Adicionar arquivos de projeto.

---
+++ WF_MySql_pizzerria/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WF_MySql_pizzerria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nascimento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Info;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.nascimento,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(559, 200);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nome_complero";
+            this.columnHeader1.Width = 94;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CEP";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 67;
+            // 
+            // nascimento
+            // 
+            this.nascimento.Tag = "Nascimento";
+            this.nascimento.Text = "Nascimento";
+            this.nascimento.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nascimento.Width = 137;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "DDD";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Telefone";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 79;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Complemento";
+            this.columnHeader5.Width = 112;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(559, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(583, 309);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader nascimento;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ WF_MySql_pizzerria/Form1.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_MySql_pizzerria
+{
+    public partial class Form1 : Form
+    {
+        //faze de novo essa parte pra aprende
+        MySqlConnection con;
+        MySqlCommand command;
+        MySqlDataReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new MySqlConnection("Server=localhost;Database=pizzeriadb;User=root;Pwd=;SslMode=none");
+        }
+
+        class Dados
+        {
+            public string nome { get; set; }
+            public DateTime  data_nascimento { get; set; }
+            public string telefone { get; set; }
+            public string ddd { get; set; }
+            public string complemento { get; set; }
+            public string CEP { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Dados> listaNomes = new List<Dados>();
+            //Popular a lista atraves do select do banco de dados
+            command = new MySqlCommand();
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "Select nome_completo,data_nascimento,telefone,ddd,complemento,CEP from cliente";
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Dados nome = new Dados();
+
+                nome.nome = reader["nome_completo"].ToString();
+                nome.data_nascimento = Convert.ToDateTime(reader["data_nascimento"].ToString());
+                nome.telefone = reader["telefone"].ToString();
+                nome.ddd = reader["ddd"].ToString();
+                nome.complemento = reader["complemento"].ToString();
+                nome.CEP = reader["CEP"].ToString();
+
+                ListViewItem Nome = new ListViewItem();
+
+
+                Nome.Text = nome.nome;
+                Nome.SubItems.Add(nome.CEP);
+                Nome.SubItems.Add(nome.data_nascimento.ToString("dd/MM/yyyy"));
+                Nome.SubItems.Add(nome.ddd);
+                Nome.SubItems.Add(nome.telefone);
+                Nome.SubItems.Add(nome.complemento);
+                
+
+                listView1.Items.Add(Nome);
+
+            }
+            con.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
